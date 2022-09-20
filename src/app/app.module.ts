9dfdd85b85068405442ed234ab9bea9eba5d1077
adsadilson <adilson.curso@yahoo.com.br>
2022-09-20 17:09:16 -0300

    Criação do footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,10 +20,11 @@ import { MatTableModule } from '@angular/material/table'
 import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { HeaderComponent } from './views/components/template/header/header.component'
+import { HeaderComponent } from './views/components/template/header/header.component';
+import { FooterComponent } from './views/components/template/footer/footer.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
