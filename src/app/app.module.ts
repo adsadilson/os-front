09297d21d577be67f17aa05a138597891d5edc72
adsadilson <adilson.curso@yahoo.com.br>
2022-09-20 17:37:34 -0300

    Criação do sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,10 +21,11 @@ import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { HeaderComponent } from './views/components/template/header/header.component';
-import { FooterComponent } from './views/components/template/footer/footer.component'
+import { FooterComponent } from './views/components/template/footer/footer.component';
+import { NavComponent } from './views/components/template/nav/nav.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
